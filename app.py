42b456521aba27e3b51179d5285a052c5ee35726
Mohammed Sadiq Alali <54593398+Mohammed-S-Alali@users.noreply.github.com>
2021-06-19 21:57:06 +0300

    Initial commit without data

--- app.py
+++ app.py
@@ -12,54 +12,78 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
 
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:M159@localhost:5432/fyyur'
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
 # TODO: connect to a local postgresql database
 
+migrate = Migrate(app, db)
+
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
 
 class Venue(db.Model):
-    __tablename__ = 'Venue'
+  __tablename__ = 'Venue'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String)
+  city = db.Column(db.String(120))
+  state = db.Column(db.String(120))
+  address = db.Column(db.String(120))
+  phone = db.Column(db.String(120))
+  image_link = db.Column(db.String(500))
+  facebook_link = db.Column(db.String(120))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-    is_seeking_talent = db.Column(db.Boolean, default=False)
-    
+  # TODO: implement any missing fields, as a database migration using Flask-Migrate
+  website_link = db.Column(db.String(120))
+  is_seeking_talent = db.Column(db.Boolean, default=False)
+  seeking_description = db.Column(db.String(480), default='')
+  shows = db.relationship('Show', backref='venue', lazy=True)
+
+  def __repr__(self):
+        return f'<Venue ID: {self.id}, name: {self.name}, city: {self.city}>'
+
 class Artist(db.Model):
-    __tablename__ = 'Artist'
+  __tablename__ = 'Artist'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String)
+  city = db.Column(db.String(120))
+  state = db.Column(db.String(120))
+  phone = db.Column(db.String(120))
+  genres = db.Column(db.String(120))
+  image_link = db.Column(db.String(500))
+  facebook_link = db.Column(db.String(120))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+  # TODO: implement any missing fields, as a database migration using Flask-Migrate
+  website_link = db.Column(db.String(120))
+  is_seeking_venue = db.Column(db.Boolean, default=False)
+  seeking_description = db.Column(db.String(480), default='')
+  shows = db.relationship('Show',backref='artist', lazy=True)
+
+  def __repr__(self):
+        return f'<Artist ID: {self.id}, name: {self.name}, city: {self.city}>'
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+  __tablename__ = 'Show'
 
+  id = db.Column(db.Integer, primary_key=True)
+  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+  start_datetime = db.Column(db.DateTime, nullable=False)
+
+  def __repr__(self):
+        return f'<Show ID: {self.id}, Venue Id: {self.venue_id}, Artist Id: {self.city}>'
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -91,6 +115,7 @@ def index():
 def venues():
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
+  
   data=[{
     "city": "San Francisco",
     "state": "CA",
